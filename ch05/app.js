677385facd0f549ed2f5827c8711d7f478edc883
novdov <rpwjd56@gmail.com>
2020-04-29 08:40:05 +0900

    ch05) Add example

---
+++ ch05/app.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express()
+
+app.use('/add-product', (req, res, next) => {
+  console.log('In another middleware!')
+  res.send('<h1>Add product</h1>')
+})
+
+app.use('/', (req, res, next) => {
+  console.log('In another middleware!')
+  res.send('<h1>Hello from Express!</h1>')
+})
+
+app.listen(3000)
