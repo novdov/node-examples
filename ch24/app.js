d0e5cd5394735cfe4ede8794ce85ec4178ed9d7f
SunWoong KIM <35526032+novdov@users.noreply.github.com>
2020-05-03 21:02:03 +0900

    Update app.js

--- ch24/app.js
+++ ch24/app.js
@@ -31,6 +31,8 @@ app.use(bodyParser.json())
 app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'))
 app.use('/images', express.static(path.join(__dirname, 'images')))
 app.use((req, res, next) => {
+  // Handle CORSS Error
+  // Allow frontend send request to server
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
